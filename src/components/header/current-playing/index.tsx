ce6453a0bc65d89f401690b53c89825f96aac699
mrLuisFer <mrluisfeer@gmail.com>
2025-04-18 19:08:42 -0600

    update spotify album cover

--- src/components/header/current-playing/index.tsx
+++ src/components/header/current-playing/index.tsx
@@ -1,13 +1,48 @@
-import spotifyAnimationData from '@/lotties/spotify.json';
 import Lottie, { type LottieRefCurrentProps } from 'lottie-react';
 import { AnimatePresence } from 'motion/react';
 import * as motion from 'motion/react-client';
-import { useRef, useState } from 'react';
+import { useRef, useState, type ReactNode } from 'react';
 import { TbMusicPause } from 'react-icons/tb';
+import colorVioletCover from '@/assets/albums/color-violet.webp';
+import spotifyAnimationData from '@/lotties/spotify.json';
 
 export default function CurrentPlaying() {
-  const [showPopover, setShowPopover] = useState(false);
   const spotifyRefAnimationData = useRef<LottieRefCurrentProps>(null);
+
+  return (
+    <Popover>
+      <div
+        className={`flex relative items-center justify-center gap-2 w-[300px] h-[300px] rounded-xl flex-col`}
+      >
+        <div className="flex items-center absolute top-3 gap-2 left-3">
+          <div className="relative h-5 w-5 overflow-hidden">
+            <Lottie
+              lottieRef={spotifyRefAnimationData}
+              animationData={spotifyAnimationData}
+              className="absolute left-0 h-fit w-fit transform"
+              autoplay={false}
+              loop={false}
+            />
+          </div>
+          <p className="text-xs text-white font-semibold">My favorites</p>
+        </div>
+        <img src={colorVioletCover.src} alt="The Color Violet" className="rounded-xl" />
+        <motion.div
+          className="absolute bottom-5 w-[280px] bg-yellow-500/10 backdrop-blur-md text-white px-4 py-3 rounded-xl"
+          whileHover={{
+            scale: 0.95,
+          }}
+        >
+          <p className="font-bold text-lg">The Color Violet</p>
+          <p className="text-sm">Tory Lanez</p>
+        </motion.div>
+      </div>
+    </Popover>
+  );
+}
+
+function Popover({ children }: { children: ReactNode }) {
+  const [showPopover, setShowPopover] = useState(true);
 
   return (
     <motion.div>
@@ -20,25 +55,14 @@ export default function CurrentPlaying() {
         <AnimatePresence mode="wait">
           {showPopover && (
             <motion.div
-              className="absolute top-7 -left-10 z-20 min-w-[170px] rounded-xl border border-neutral-400 bg-white px-2 py-3 shadow-md"
+              className="absolute top-7 -left-10 z-20 min-w-[170px] rounded-xl border border-neutral-400 bg-white shadow-md"
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0.8 }}
               transition={{ duration: 0.2 }}
               key="box"
             >
-              <div className="flex items-center justify-center gap-2">
-                <div className="relative h-7 w-7 overflow-hidden">
-                  <Lottie
-                    lottieRef={spotifyRefAnimationData}
-                    animationData={spotifyAnimationData}
-                    className="absolute left-0 h-fit w-fit transform"
-                    autoplay={false}
-                    loop={false}
-                  />
-                </div>
-                <p className="text-xs">Trending on Spotify</p>
-              </div>
+              {children}
             </motion.div>
           )}
         </AnimatePresence>
