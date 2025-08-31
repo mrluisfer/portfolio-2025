a2ac4e89e60570841afae8e4ed1cf6c7b2bc4e20
mrLuisFer <mrluisfeer@gmail.com>
2025-08-31 15:05:50 -0600

    refactor: optimize Skills component for better device resource handling

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -5,15 +5,27 @@ import '@/styles/technologies-shadow.scss';
 
 import { motion } from 'motion/react';
 
-import Container from '../container';
-import Technologies from './technologies';
 import { NAVIGATION_NAMES } from '@/constants/navigation-names';
+import { useDeviceResources } from '@/hooks/use-device-resources';
+import { cn } from '@/lib/utils';
+import Container from '../container';
 import Title from '../title';
+import Technologies from './technologies';
 
 export default function Skills() {
+  const deviceResources = useDeviceResources();
+
   return (
     <Container>
-      <div className="general-pattern technologies-shadow-y my-6 p-10" id={NAVIGATION_NAMES.SKILLS}>
+      <div
+        className={cn(
+          'technologies-shadow-y my-6 p-10',
+          deviceResources?.deviceMemoryGB !== undefined && deviceResources.deviceMemoryGB > 8
+            ? 'general-pattern'
+            : ''
+        )}
+        id={NAVIGATION_NAMES.SKILLS}
+      >
         <Title>
           <motion.h1
             className="absolute -top-[30px] z-20 drop-shadow-2xl"
