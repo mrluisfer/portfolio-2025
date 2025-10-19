0ef70f50bedfeeb4164d07f0421d9df4d74abbca
Luis Alvarez <mrluisfeer@gmail.com>
2025-10-18 23:21:16 -0600

    feat: update About component to reflect Full Stack Developer role and add Lottie animation

--- src/app/_components/about/index.tsx
+++ src/app/_components/about/index.tsx
@@ -4,6 +4,9 @@ import { motion } from 'motion/react';
 import FocusedOn from '@/app/_components/about/focused-on';
 import { NAVIGATION_NAMES } from '@/constants/navigation-names';
 import Container from '../../../components/container';
+import Lottie from 'lottie-react';
+import craft from '@/lotties/craft.json';
+import { LayoutDashboardIcon, ServerIcon } from 'lucide-react';
 
 export default function About() {
   return (
@@ -28,7 +31,26 @@ export default function About() {
             scale: 0.9,
           }}
         >
-          Front-end focused Software Developer
+          <p className={'font-bold'}>Full Stack Developer</p>
+          <p className={'flex mt-1 flex-wrap items-start gap-2 justify-center text-lg'}>
+            <div className={'flex items-center'}>
+              <div className="h-7 w-8">
+                <Lottie animationData={craft} />
+              </div>
+              Crafting
+            </div>{' '}
+            both
+            <span className={'flex gap-1 items-center text-yellow-400'}>
+              <LayoutDashboardIcon />
+              Front-end
+            </span>{' '}
+            and
+            <span className={'flex gap-1 items-center text-violet-400'}>
+              <ServerIcon />
+              Back-end
+            </span>{' '}
+            solutions
+          </p>
         </motion.div>
         <span className="mx-auto block w-[350px] text-lg font-medium opacity-70 sm:w-[400px] lg:w-[800px] lg:text-3xl">
           specialized in designing and building scalable, maintainable, user-centric web apps
