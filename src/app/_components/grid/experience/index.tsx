ef0cfb3118c2c7ccac7b04e7c8140fa6d6ff8fc3
Luis Alvarez <mrluisfeer@gmail.com>
2025-10-19 00:45:29 -0600

    feat: implement scrollable area for ExperienceTimeline and adjust layout in Experience component

--- src/app/_components/grid/experience/index.tsx
+++ src/app/_components/grid/experience/index.tsx
@@ -1,49 +1,15 @@
 import ExperienceTimeline from '@/components/shared/experience-timeline';
-import { type ReactNode } from 'react';
-import { type TExperience } from './timeline-experience';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 export default function Experience() {
   return (
-    <div className="flex w-full flex-1 flex-col justify-center overflow-y-scroll rounded-[inherit] bg-gradient-to-tr from-blue-50 to-blue-100 p-2 group-hover:bg-neutral-100 dark:from-blue-950 dark:to-zinc-900">
-      <ExperienceTimeline minimal className="h-full" />
+    <div className="flex w-full flex-1 flex-col justify-center rounded-[inherit] bg-gradient-to-tr from-blue-50 to-blue-100 p-2 group-hover:bg-neutral-100 dark:from-blue-950 dark:to-zinc-900">
+      <ScrollArea
+        type={'always'}
+        className="h-[400px] w-full rounded-[inherit] md:h-[500px] lg:h-full"
+      >
+        <ExperienceTimeline minimal className="h-full p-1" />
+      </ScrollArea>
     </div>
-  );
-}
-
-function LightText({
-  children,
-  size = 'sm',
-}: {
-  children: ReactNode | string;
-  size?: 'sm' | 'xs';
-}) {
-  const sizeClassName = {
-    sm: 'text-sm',
-    xs: 'text-xs',
-  };
-  return (
-    <span
-      className={`text-neutral-500 transition-colors hover:text-neutral-600 dark:text-neutral-400 dark:hover:text-neutral-200 ${sizeClassName[size]}`}
-    >
-      {children}
-    </span>
-  );
-}
-
-function TimelineItem({ experience }: { experience: TExperience }) {
-  return (
-    <li className="relative flex flex-1 items-center justify-start gap-2 py-3 pl-2">
-      <div className="absolute left-[11px] h-full w-0.5 grow bg-neutral-900"></div>
-      <div className="relative z-20 block h-2 w-2 animate-pulse rounded-full bg-neutral-800"></div>
-      <div className="flex flex-col">
-        <p className="font-semibold">
-          {experience.title} <LightText>- {experience.company}</LightText>
-        </p>
-        <LightText size="xs">{experience.date}</LightText>
-        <LightText size="sm">
-          {experience.time} - {experience.type}
-        </LightText>
-      </div>
-    </li>
   );
 }
--- src/app/_components/grid/index.tsx
+++ src/app/_components/grid/index.tsx
@@ -27,7 +27,7 @@ export default function Grid() {
           </Card>
 
           {/* Sharing */}
-          <Card className="w-full lg:row-span-3 lg:row-start-4">
+          <Card className="w-full lg:row-span-3 lg:row-start-4 md:justify-start">
             <Sharing />
           </Card>
 
