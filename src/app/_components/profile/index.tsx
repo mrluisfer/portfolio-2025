4437eb4141ee9bc3b2515a8d1339f5dc4ed489a2
Luis Alvarez <mrluisfeer@gmail.com>
2025-10-26 23:52:37 -0600

    refactor: enhance profile components with motion effects and improved layout; remove unnecessary horizontal rule

--- src/app/_components/profile/index.tsx
+++ src/app/_components/profile/index.tsx
@@ -15,7 +15,6 @@ export default function Profile() {
         <div id={NAVIGATION_NAMES.PROFILE}>
           <Header />
           <Experience />
-          <hr />
           <AboutMe />
         </div>
       </Container>
