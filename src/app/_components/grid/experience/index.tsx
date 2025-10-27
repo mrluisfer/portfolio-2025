ea7882cf8d310c67676447580f535a718388c2fe
Luis Alvarez <mrluisfeer@gmail.com>
2025-10-26 23:24:16 -0600

    fix: update ScrollArea height to use 100% for better responsiveness

--- src/app/_components/grid/experience/index.tsx
+++ src/app/_components/grid/experience/index.tsx
@@ -6,7 +6,7 @@ export default function Experience() {
     <div className="flex w-full flex-1 flex-col justify-center rounded-[inherit] bg-gradient-to-tr from-blue-50 to-blue-100 p-2 group-hover:bg-neutral-100 dark:from-blue-950 dark:to-zinc-900">
       <ScrollArea
         type={'always'}
-        className="h-[400px] w-full rounded-[inherit] md:h-[500px] lg:h-full"
+        className="h-[100%] w-full rounded-[inherit] md:h-[500px] lg:h-full"
       >
         <ExperienceTimeline minimal className="h-full p-1" />
       </ScrollArea>
